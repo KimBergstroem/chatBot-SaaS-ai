c9fd83660842beea75848eae514e2ec3707316ad
KimBergstroem <kimmen_b@hotmail.com>
2024-08-02 17:33:00 +0200

    feat(Routes): add routes for Chat and Users

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config();
-
-//routes
 const app = express();
 
 //middlewares
 app.use(express.json());
+
+//remove it in production environment
+app.use(morgan("dev"));
+
+app.use("/api/v1", appRouter);
 
 export default app;
