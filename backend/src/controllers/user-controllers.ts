96152b922dbabe7e1fe39fe83818552ba03df051
KimBergstroem <kimmen_b@hotmail.com>
2024-08-04 17:22:50 +0200

    feat(auth): implement JWT token creation and cookie handling

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { COOKIE_NAME } from "../utils/constants.js";
+import { createToken } from "../utils/token-manager.js";
 import { hash, compare } from "bcrypt"; // Encrypt password
 
 export const getAllUsers = async (
@@ -31,6 +33,31 @@ export const userSignUp = async (
     const hashedPassword = await hash(password, 10); // Generating encoded password and store it in hashedPassword
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    //create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+
+    /* 
+    Send the token to frontend and sets it as a cookie
+    Change localhost variable to the server domain when deployed
+    */
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.error(error.message);
@@ -54,6 +81,30 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send({ message: "Incorrect Password" });
     }
+
+    //create token and store cookie
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: "localhost",
+      signed: true,
+      path: "/",
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    /* 
+    Send the token to frontend and sets it as a cookie
+    Change localhost variable to the server domain when deployed
+    */
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.error(error.message);
