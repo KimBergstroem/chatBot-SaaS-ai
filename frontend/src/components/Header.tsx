3525b9806a7bd66b68e6b3d6f13f14d315c417d2
KimBergstroem <kimmen_b@hotmail.com>
2024-08-05 13:36:49 +0200

    feat(Header): add NavLink components with basic CSS styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,13 +2,51 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavLink from "./shared/NavLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavLink
+                bg="#F88379"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavLink
+                bg="#008080"
+                to="/"
+                text="Logout"
+                textColor="white"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavLink
+                bg="#F88379"
+                to="/login"
+                text="Login"
+                textColor="white"
+              />
+              <NavLink
+                bg="#008080"
+                to="/signup"
+                text="Signup"
+                textColor="white"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
