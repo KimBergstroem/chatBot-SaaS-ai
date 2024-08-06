20a55a787957ab109fa45c1af5ad35c6b465f151
KimBergstroem <kimmen_b@hotmail.com>
2024-08-06 16:32:59 +0200

    feat: create seperate ChatItem component for chat messages

---
+++ frontend/src/components/chat/ChatItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Box, Avatar, Typography } from "@mui/material";
+
+const ChatItem = ({
+  content,
+  role,
+}: {
+  content: string;
+  role: "user" | "assistant";
+}) => {
+  return role === "assistant" ? (
+    <Box sx={{ display: "flex", p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
+      <Avatar sx={{ ml: "0" }}>
+        <img src="bot-icon.png" alt="openai" width="30px" />
+      </Avatar>
+      <Box>
+        <Typography fontSize={"20px"}>{content}</Typography>
+      </Box>
+    </Box>
+  ) : (
+    <Box sx={{ display: "flex", p: 2, bgcolor: "#F88379", gap: 2 }}>
+      <Avatar
+        sx={{ ml: "0", bgcolor: "black", color: "white", fontSize: "13px" }}>
+        YOU
+      </Avatar>
+      <Box>
+        <Typography fontSize={"20px"}>{content}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default ChatItem;
