5a1b3976200ec8272e9375412f02e05eec19d2e3
KimBergstroem <kimmen_b@hotmail.com>
2024-08-06 15:09:25 +0200

    feat: implement OpenAI API configuration and chat handling

---
+++ backend/src/controllers/chat-controllers.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import { ERROR_MESSAGES, SUCCESS_MESSAGES } from "../utils/constants.js";
+import { configureOpenAI } from "../config/openai-config.js";
+import { OpenAIApi, ChatCompletionRequestMessage } from "openai";
+import User from "../models/User.js";
+
+export const generateChatCompletion = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { message } = req.body;
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user)
+      return res
+        .status(401)
+        .json({ message: ERROR_MESSAGES.USER_NOT_REGISTERED });
+
+    //Grab chats of user
+    const chats = user.chats.map(({ role, content }) => ({
+      role,
+      content,
+    })) as ChatCompletionRequestMessage[];
+    chats.push({ content: message, role: "user" });
+    user.chats.push({ content: message, role: "user" });
+
+    //send all chats with new one to OPENAI API
+    const config = configureOpenAI();
+    const openai = new OpenAIApi(config);
+    const chatResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chats,
+    });
+    user.chats.push(chatResponse.data.choices[0].message);
+    await user.save();
+    return res
+      .status(200)
+      .json({ message: SUCCESS_MESSAGES.CHAT_COMPLETION_SUCCESS });
+    //get latest response
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR });
+  }
+};
