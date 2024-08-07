6d69d1b58f4cc759949d900dc63944466d4d7fbe
KimBergstroem <kimmen_b@hotmail.com>
2024-08-07 18:02:32 +0200

    feat(chat): add code block extraction and syntax highlights for chat messages

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import { Box, Avatar, Typography } from "@mui/material";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus as darkRedTheme } from "react-syntax-highlighter/dist/esm/styles/prism";
 
+function extractCodeFromString(message: string) {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+}
+
+function isCodeBlock(str: string) {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("/")
+  ) {
+    return true;
+  }
+  return false;
+}
 const ChatItem = ({
   content,
   role,
@@ -8,13 +32,28 @@ const ChatItem = ({
   content: string;
   role: "user" | "assistant";
 }) => {
+  const messageBlocks = extractCodeFromString(content);
   return role === "assistant" ? (
     <Box sx={{ display: "flex", p: 2, bgcolor: "#2e2e2e", my: 2, gap: 2 }}>
       <Avatar sx={{ ml: "0" }}>
         <img src="bot-icon.png" alt="openai" width="30px" />
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={darkRedTheme} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            )
+          )}
+        ;
       </Box>
     </Box>
   ) : (
@@ -24,7 +63,21 @@ const ChatItem = ({
         YOU
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={darkRedTheme} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            )
+          )}
+        ;
       </Box>
     </Box>
   );
