16f13e65953b67fdb1e6d8c73f9253a674397494
KimBergstroem <kimmen_b@hotmail.com>
2024-08-12 19:33:24 +0200

    chore: replaced the basic vite logo with custom made logo

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -12,22 +12,24 @@ const Logo = () => {
         gap: "15px",
       }}>
       <Link to={"/"}>
-        <img
-          src="vite.svg"
-          alt="openai"
-          width={"30px"}
-          height={"30px"}
-          className="image-inverted"
-        />
+        {
+          <img
+            src="logo.png"
+            alt="openai"
+            width={"75px"}
+            height={"75px"}
+            className="image-inverted"
+          />
+        }
       </Link>
       <Typography
         sx={{
-          display: { md: "block", sm: "block", xs: "none" },
+          display: { md: "block", sm: "none", xs: "none" },
           mr: "auto",
           fontWeight: "800",
           textShadow: "2px 2px 20px #000",
         }}>
-        <span style={{ fontSize: "24px" }}>GameBot</span>-Nexus
+        <span style={{ fontSize: "24px" }}>GameIn</span> - Let's connect
       </Typography>
     </div>
   );
