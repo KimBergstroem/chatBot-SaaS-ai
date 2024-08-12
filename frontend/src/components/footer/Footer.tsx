bb114b3e62ce4ce5e286e51f6f4fc773219b10f5
KimBergstroem <kimmen_b@hotmail.com>
2024-08-12 19:28:54 +0200

    feat(footer): create component and integrate into Home page

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          padding: 20,
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 70,
+        }}>
+        <p style={{ fontSize: "30px", textAlign: "center" }}>
+          Author -
+          <Link
+            className="nav-link"
+            target="_blank"
+            to={"https://github.com/KimBergstroem"}>
+            Github
+          </Link>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
